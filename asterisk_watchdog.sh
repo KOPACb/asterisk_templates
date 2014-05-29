884c8bf7fd89e49fe07684a9877d12f0023c4922
The KOPACb <KOPACb@gmail.com>
2014-05-29 06:33:36 -0700

    Update asterisk_watchdog.sh

--- asterisk_watchdog.sh
+++ asterisk_watchdog.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 #asterisk watchdog
 #/usr/local/bin/asterisk-watchdog.sh
-#crontab:
+#do not forget set chmod +x
+#crontab line:
 # * * * * * /usr/local/bin/asterisk-watchdog.sh
-
-#set -x
-
 /usr/sbin/asterisk -rx 'core show version' >> /dev/null
 errcode=$?
 
@@ -16,8 +14,6 @@ if [ "$errcode" != "0" ] ;then
 echo "[$(date)]Started Asterisk Service at $(date)" >> /var/log/asterisk/asterisk-check.log
 fi
 
-
-#set +x
 exit
 
 
